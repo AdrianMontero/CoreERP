90fa4d613d0c21ec69d6b893263eef07436900af
DanielSanchezSanchidrian <dnelsanchez@gmail.com>
2017-01-11 17:48:37 +0100

    AtributosEmpleado
    atributos y constructores de empleado

--- src/ddda/erp/objetos/Empleado.java
+++ src/ddda/erp/objetos/Empleado.java
@@ -5,10 +5,70 @@
  */
 package ddda.erp.objetos;
 
+import ddda.erp.core.CoreBD;
+import java.sql.ResultSet;
+
 /**
  *
  * @author juanxxiii
  */
 public class Empleado {
+    
+    //Metodos para interactuar con la BD.
+    private CoreBD bd = new CoreBD();
+    private String sql = null;
+    private ResultSet rs = null;
+    
+    //Atributos del empleado.
+    private int idEmpleado;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String fechaInicio;
+    private String cargo;
+    private int idCine;
+
+    /**
+     * Construtor del empleado con el idEmpleado que no sera utilizado al crear 
+     * un nuevo cine puesto que el id es autoincremental. 
+     * @param idEmpleado id del empleado.
+     * @param dni dni del empleado.
+     * @param nombre nombre del empleado.
+     * @param apellido apellidos del empleado.
+     * @param fechaInicio fecha de inicio de alta.
+     * @param cargo puesto que ocupa el empleado.
+     * @param idCine id del cine al que esta asociado.
+     */
+    public Empleado(int idEmpleado, String dni, String nombre, String apellido, String fechaInicio, String cargo, int idCine) {
+        this.idEmpleado = idEmpleado;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaInicio = fechaInicio;
+        this.cargo = cargo;
+        this.idCine = idCine;
+    }
+    /**
+     * Construtor del empleado con el idEmpleado que no sera utilizado al crear 
+     * un nuevo cine puesto que el id es autoincremental.
+     * @param dni dni del empleado.
+     * @param nombre nombre del empleado.
+     * @param apellido apellidos del empleado.
+     * @param fechaInicio fecha de inicio de alta.
+     * @param cargo puesto que ocupa el empleado.
+     * @param idCine id del cine al que esta asociado. 
+     */
+    public Empleado(String dni, String nombre, String apellido, String fechaInicio, String cargo, int idCine) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaInicio = fechaInicio;
+        this.cargo = cargo;
+        this.idCine = idCine;
+    }
+    
+    
+    
+    
     
 }
