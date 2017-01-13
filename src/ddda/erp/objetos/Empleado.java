cde243b3ed7ee4c2c01ef9ab0bf4c64217398314
DanielSanchezSanchidrian <dnelsanchez@gmail.com>
2017-01-13 17:45:23 +0100

    metodos empleado
    crear, mostrarEmpleados, mostrarEmpleadoID, mostrarEmpleadoNombre,
    borrarEmpleadoID, modificarEmpleado

--- src/ddda/erp/objetos/Empleado.java
+++ src/ddda/erp/objetos/Empleado.java
@@ -7,6 +7,7 @@ package ddda.erp.objetos;
 
 import ddda.erp.core.CoreBD;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -26,6 +27,8 @@ public class Empleado {
     private String apellido;
     private String fechaInicio;
     private String cargo;
+    private String usuario;
+    private String contrasena;
     private int idCine;
 
     /**
@@ -37,17 +40,22 @@ public class Empleado {
      * @param apellido apellidos del empleado.
      * @param fechaInicio fecha de inicio de alta.
      * @param cargo puesto que ocupa el empleado.
+     * @param usuario usuario de sesión.
+     * @param contrasena contraseña de sesión.
      * @param idCine id del cine al que esta asociado.
      */
-    public Empleado(int idEmpleado, String dni, String nombre, String apellido, String fechaInicio, String cargo, int idCine) {
+    public Empleado(int idEmpleado, String dni, String nombre, String apellido, String fechaInicio, String cargo, String usuario, String contrasena, int idCine) {   
         this.idEmpleado = idEmpleado;
         this.dni = dni;
         this.nombre = nombre;
         this.apellido = apellido;
         this.fechaInicio = fechaInicio;
         this.cargo = cargo;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
         this.idCine = idCine;
     }
+
     /**
      * Construtor del empleado con el idEmpleado que no sera utilizado al crear 
      * un nuevo cine puesto que el id es autoincremental.
@@ -56,19 +64,113 @@ public class Empleado {
      * @param apellido apellidos del empleado.
      * @param fechaInicio fecha de inicio de alta.
      * @param cargo puesto que ocupa el empleado.
+     * @param usuario usuario de sesión.
+     * @param contrasena contraseña de sesión.
      * @param idCine id del cine al que esta asociado. 
      */
-    public Empleado(String dni, String nombre, String apellido, String fechaInicio, String cargo, int idCine) {
+    public Empleado(String dni, String nombre, String apellido, String fechaInicio, String cargo, String usuario, String contrasena, int idCine) {
         this.dni = dni;
         this.nombre = nombre;
         this.apellido = apellido;
         this.fechaInicio = fechaInicio;
         this.cargo = cargo;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
         this.idCine = idCine;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public int getIdCine() {
+        return idCine;
+    }
+
+    
+    
+    
+    
+    
+    /**
+     * Creamos un nuevo empleado.
+     * @param _empleado
+     * @throws SQLException 
+     */
+    public void crearEmpleado(Empleado _empleado) throws SQLException {
+        bd.actualizarTabla("insert into empleados values(null, " + _empleado.getDni()+ ", " + _empleado.getNombre()+ ", " + _empleado.getApellido()+ ", " + _empleado.getFechaInicio()+ ", " + _empleado.getCargo() + ", " + _empleado.getUsuario()+ ", " + _empleado.getContrasena()+", " + _empleado.getIdCine()+ ")");
+    }
+    
+    /**
+     * Muestra todos los empleados.
+     * @param _empleado
+     * @throws SQLException 
+     */
+    public void mostrarEmpleados(Empleado _empleado) throws SQLException {
+        bd.consultarTabla("Select * from empleado");
+    }
+    /**
+     * Muestra empleado segun id.
+     * @param _empleado
+     * @throws SQLException 
+     */
+    public void mostrarEmpleadoID(Empleado _empleado) throws SQLException {
+        bd.consultarTabla("Select * from empleado where idEmpleado ="+_empleado.getIdEmpleado()+"");
+    }
+    /**
+     * Busca empleado segun nombre.
+     * @param _empleado
+     * @throws SQLException 
+     */
+    public void mostrarEmpleadoNombre(Empleado _empleado) throws SQLException {
+        bd.consultarTabla("Select * from empleado where nombre_emp ="+_empleado.getNombre()+"");
     }
     
     
-    
+    /**
+     * Borrar empleado segun id.
+     * @param _empleado
+     * @throws SQLException 
+     */
+    public void borrarEmpleadoID(Empleado _empleado) throws SQLException {
+        bd.consultarTabla("Delete * from empleado where idEmpleado ="+_empleado.getIdEmpleado()+"");
+    }
+   /**
+    * Modiica un empleado.
+    * @param _empleado
+    * @throws SQLException 
+    */
+    public void modificarEmpleado(Empleado _empleado) throws SQLException {
+        bd.consultarTabla("Update empleado set dni_emp="+_empleado.getDni()+",nombre_emp="+_empleado.getNombre()+",apellidos_emp="+_empleado.getApellido()+",fechaInicio_emp="+_empleado.getFechaInicio()+",cargo_emp="+_empleado.getCargo()+",usuario_emp="+_empleado.getUsuario()+",contrasenia_emp="+_empleado.getContrasena()+",idCine="+_empleado.getIdCine()+"");
+    }
     
     
 }
