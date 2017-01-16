816f16ab00d1b80fd01d0ba8297f2fc385e86388
DanielSanchezSanchidrian <dnelsanchez@gmail.com>
2017-01-16 18:12:42 +0100

    productos

---
+++ src/ddda/erp/objetos/Producto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ddda.erp.objetos;
+
+/**
+ *
+ * @author juanxxiii
+ */
+public class Producto {
+    
+    private int idProducto;
+    private String descripcion;
+    private int pecio;
+    private String nombre;
+    private int stock;
+}
